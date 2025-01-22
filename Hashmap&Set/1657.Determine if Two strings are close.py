60dc8ffbc5a111b988ef848ecb68969fca0872aa
CHPT-RD-Equipment-Department1-AI <bosen.wang@cht-pt.com.tw>
2025-01-22 16:54:08 +0800

    upgrade 1657.Determine if Two strings are close.py

--- Hashmap&Set/1657.Determine if Two strings are close.py
+++ Hashmap&Set/1657.Determine if Two strings are close.py
@@ -33,57 +33,13 @@ class Solution:
         return (word1_type == word2_type) and (word1_num == word2_num)
 
 if __name__ == '__main__':
-    # s = Solution()
-    # word1 = 'aaabbbbccddeeeeefffff'
-    # word2 = 'aaaaabbcccdddeeeeffff'
-    # print(s.closeStrings(word1, word2))
+    s = Solution()
+    word1 = 'aaabbbbccddeeeeefffff'
+    word2 = 'aaaaabbcccdddeeeeffff'
+    print(s.closeStrings(word1, word2))
 
-    import os
-    import matplotlib.pyplot as plt
-    import cv2
-    import numpy as np
-    import shutil
-    shutil.rmtree('C:/Users/240503/Desktop/PCB_Background')
-    if os.path.exists('C:/Users/240503/Desktop/PCB_Background'): pass
-    else: os.mkdir('C:/Users/240503/Desktop/PCB_Background')
 
-    if os.path.exists('C:/Users/240503/Desktop/PCB_Background/train'): pass
-    else:
-        os.mkdir('C:/Users/240503/Desktop/PCB_Background/train_HR')
-        os.mkdir('C:/Users/240503/Desktop/PCB_Background/train_LR')
 
-    if os.path.exists('C:/Users/240503/Desktop/PCB_Background/test'): pass
-    else:
-        os.mkdir('C:/Users/240503/Desktop/PCB_Background/test_HR')
-        os.mkdir('C:/Users/240503/Desktop/PCB_Background/test_LR')
-
-    def sliding_window(image, h, w):
-        images = []
-        for x in range(0, w, 640):
-            for y in range(0, h, 640):
-                crop_image = image[y: y+640, x: x+640]
-                if crop_image.shape != (640, 640, 3): continue
-                images.append(crop_image)
-        return np.array(images)
-
-    path = 'D:/PCB defect detection/PCB_Automation_crop/PCBA_V9300/'
-    for num, filename in enumerate(os.listdir(path)):
-        if num > 5: break
-        image = cv2.imread(path + filename)
-        h, w = image.shape[0], image.shape[1]
-        subimages = sliding_window(image, h, w)
-        print(subimages.shape)
-        if num < 5:
-            for subnum, image in enumerate(subimages):
-                L = cv2.resize(image, (160, 160), cv2.INTER_CUBIC)
-                cv2.imwrite(f'C:/Users/240503/Desktop/PCB_Background/train_HR/{num}_{subnum}.jpg', image)
-                cv2.imwrite(f'C:/Users/240503/Desktop/PCB_Background/train_LR/{num}_{subnum}.jpg', L)
-
-        if num == 5:
-            for subnum, image in enumerate(subimages):
-                L = cv2.resize(image, (160, 160), cv2.INTER_CUBIC)
-                cv2.imwrite(f'C:/Users/240503/Desktop/PCB_Background/test_HR/{num}_{subnum}.jpg', image)
-                cv2.imwrite(f'C:/Users/240503/Desktop/PCB_Background/test_LR/{num}_{subnum}.jpg', L)
 
 
 
