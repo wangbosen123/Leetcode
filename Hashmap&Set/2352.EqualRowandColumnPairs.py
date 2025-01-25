e3c408a5d698eaddfe39506ab52e3bd22a17c99e
wangbosen123 <wang80422233032@gmail.com>
2025-01-25 20:55:34 +0800

    commit 2352 twice

--- Hashmap&Set/2352.EqualRowandColumnPairs.py
+++ Hashmap&Set/2352.EqualRowandColumnPairs.py
@@ -15,6 +15,22 @@ class Solution:
             count += rmap.count(col)
         return count
 
+class Solution:
+    def equalPairs(self, grid):
+        n = len(grid)
+        count = 0
+        rows = {}
+
+        for r in range(n):
+            row = tuple(grid[r])
+            rows[row] = 1 + rows.get(row, 0)
+
+
+        for c in range(n):
+            col = tuple(grid[i][c] for i in range(n))
+            count += rows.get(col, 0)
+
+        return count
 
 
 if __name__ == '__main__':
