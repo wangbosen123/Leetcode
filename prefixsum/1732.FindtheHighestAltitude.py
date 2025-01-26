65a9379f8a544271bf72bde701b9549cd49fef7e
wangbosen123 <wang80422233032@gmail.com>
2025-01-26 12:55:17 +0800

    commit leetcode 1732.

---
+++ prefixsum/1732.FindtheHighestAltitude.py
@@ -0,0 +1,14 @@
+class Solution:
+    def equalPairs(self, grid):
+        rmap, cmap = {}, {}
+
+        print(len(gird))
+
+if __name__ == '__main__':
+    gird = [[3, 1, 2, 2],
+            [1, 4, 4, 5],
+            [2, 4, 2, 2],
+            [2, 4, 2, 2]]
+
+    s = Solution()
+    print(s.equalPairs(grid))
